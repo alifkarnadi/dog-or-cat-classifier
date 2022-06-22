6605b1ac915c65fd69250fe5296375455036aaec
Alif Karnadi Yulvianto <karnadialif@gmail.com>
2022-06-22 20:32:01 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,9 @@
 mkdir -p ~/.streamlit/
 
-  
 echo "\
-
 [server]\n\
-
-port = $PORT\n\
-
-enableCORS = false\n\
-
 headless = true\n\
-
+port = $PORT\n\
+enableCORS = false\n\
 \n\
-
 " > ~/.streamlit/config.toml
